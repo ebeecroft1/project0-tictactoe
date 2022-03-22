bcc23bc712427256ee8452eeb96115bbf5533e71
ebeecroft1 <eric.beecroft@gmail.com>
2022-03-22 15:11:53 +1100

    First deploy

--- tictactoe/JS/board.js
+++ tictactoe/JS/board.js
@@ -1,15 +1,21 @@
 //TO DOS
 // Present which players turn it is
 // Switch turns
+// Change buttons to divs
+// Disable button/div after it has been filled
 // Display winner
 
 $('button').on('click', function () {
     $(this).text('X');
     $(this).addClass('x');
+    
+    winCheck();
 });
 
 $('button').on('dblclick', function () {
     $(this).text('O');
     $(this).removeClass('x');
     $(this).addClass('o');
+
+    winCheck();
 });
--- tictactoe/JS/game.js
+++ tictactoe/JS/game.js
@@ -1,19 +1,71 @@
 //TO DOS
 // Define win condition & write function to check for it
-const topLeft = $('#top left').text();
-const topCenter = $('#top center').text();
-const topRight = $('#top right').text();
 
+// const turnCounter = 0;
+
+const winCheck = function () { // Check if any win conditions are met
+    if ($('.top').hasClass('x') || $('.top').hasClass('o')) { // If top is all filled - check for top win
+        topWin($('.top.left').text(), $('.top.center').text(), $('.top.right').text());
+    }
+    if ($('.mid').hasClass('x') || $('.mid').hasClass('o')) { // If mid is all filled - check for mid win
+        midWin($('.mid.left').text(), $('.mid.center').text(), $('.mid.right').text());
+    }
+    if ($('.bot').hasClass('x') || $('.bot').hasClass('o')) { // If bot is all filled - check for bot win
+        botWin($('.bot.left').text(), $('.bot.center').text(), $('.bot.right').text());
+    }
+    if ($('.left').hasClass('x') || $('.left').hasClass('o')) { // If left is all filled - check for left win
+        leftWin($('.top.left').text(), $('.mid.left').text(), $('.bot.left').text());
+    }
+    if ($('.center').hasClass('x') || $('.center').hasClass('o')) { // If center is all filled - check for center win
+        centerWin($('.top.center').text(), $('.mid.center').text(), $('.bot.center').text());
+    }
+    if ($('.right').hasClass('x') || $('.right').hasClass('o')) { // If right is all filled - check for right win
+        rightWin($('.top.right').text(), $('.mid.right').text(), $('.bot.right').text());
+    }
+    if ($('.center').hasClass('x') || $('.center').hasClass('o')) { // If diagonal is all filled - check for diagonal win
+        diagonalWin($('.top.right').text(), $('.mid.center').text(), $('.bot.left').text());
+        diagonalWin($('.top.left').text(), $('.mid.center').text(), $('.bot.right').text());
+    }
+}
 
 const topWin = function(x, y, z) { // Function to check if someone won in the top row
     if (x === y && x === z) {
-        console.log(`${x} is the winner`);
-    }
-    else {
-        console.log('No winner yet');
+        console.log(`${$('.top.left').text()} is the winner`);
     }
 };
-topWin('x', 'o', 'o');
-topWin('x', 'x', 'o');
-topWin('x', 'x', 'x');
-topWin('o', 'o', 'o');
+
+const midWin = function(x, y, z) { // Function to check if someone won in the mid row
+    if (x === y && x === z) {
+        console.log(`${$('.mid.left').text()} is the winner`);
+    }
+};
+
+const botWin = function(x, y, z) { // Function to check if someone won in the bot row
+    if (x === y && x === z) {
+        console.log(`${$('.bot.left').text()} is the winner`);
+    }
+};
+
+const leftWin = function(x, y, z) { // Function to check if someone won in the left column
+    if (x === y && x === z) {
+        console.log(`${$('.top.left').text()} is the winner`);
+    }
+};
+
+const centerWin = function(x, y, z) { // Function to check if someone won in the center column
+    if (x === y && x === z) {
+        console.log(`${$('.top.center').text()} is the winner`);
+    }
+};
+
+const rightWin = function(x, y, z) { // Function to check if someone won in the right column
+    if (x === y && x === z) {
+        console.log(`${$('.top.right').text()} is the winner`);
+    }
+};
+
+const diagonalWin = function(x, y, z) { // Function to check if someone won in the right column
+    if (x === y && x === z) {
+        console.log(`${$('.mid.center').text()} is the winner`);
+    }
+};
